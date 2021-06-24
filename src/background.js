6e4144002f5831974bc60c226f0c5d6067a9103f
Ananya Gupta <67327800+ananya0208@users.noreply.github.com>
2021-06-25 00:17:10 +0800

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,7 +1,67 @@
-browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log('Hello from the background')
+function getCredentials(url, tabId) {
+  chrome.runtime.sendNativeMessage('chrome.extension',
+      { action: "get-creds", url: url },
+      function(response) {
+        if(response) {
+          if(tabId) {
+            chrome.tabs.sendMessage(tabId, { action: "fill-creds", url: response.url, credentials: response.credentials });
+          } else {
+            chrome.runtime.sendMessage({ action: "fill-creds", url: response.url, credentials: response.credentials });
+          }
+        } else {
+          chrome.runtime.sendMessage({ action: "native-app-error" , msg: "No response from native app"});
+        }
+      }
+  );
+}
 
-  browser.tabs.executeScript({
-    file: 'content-script.js',
-  });
-})
+function closePopup() {
+  var popups = chrome.extension.getViews({type: "popup"});
+  for(i = 0; i < popups.length; i++) {
+    if(popups[i].name == "chrome-pass-popup") {
+      popups[i].close();
+    }
+  }
+}
+
+function getPass(root, url, user, tabId) {
+  chrome.runtime.sendNativeMessage('chrome.extensions',
+      { action: "get-pass", user: user, path: root + "/" + user },
+      function(response) {
+        if(response) {
+          if(response.action == "fill-pass") {
+            chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+              currentTab = tabs[0];
+              chrome.tabs.sendMessage(currentTab.id, { action: "fill-pass", path: root, user: user, pass: response.pass });
+            });
+            closePopup();
+          } else {
+            console.log("Error " + response.msg);
+            chrome.runtime.sendMessage({ action: "native-app-error", msg: response.msg });
+          }
+        } else {
+          console.log("Native app returned no response");
+          chrome.runtime.sendMessage({ action: "native-app-error", msg: "No response from native app" });
+        }
+      }
+  );
+}
+
+chrome.runtime.onMessage.addListener(function(msg, sender, response) {
+  switch(msg.action) {
+    case "get-creds":
+      if(sender.tab) {
+        getCredentials(sender.tab.url, sender.tab.id);
+      } else {
+        getCredentials(msg.url);
+      }
+      return false;
+    case "get-pass":
+      if(sender.tab) {
+        getPass(msg.root, msg.url, msg.user, sender.tab.id);
+      } else {
+        getPass(msg.root, msg.url, msg.user);
+      }
+      return false;
+  }
+});
